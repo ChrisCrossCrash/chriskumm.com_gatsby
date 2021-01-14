29eea402fa943060d079ad99e0f37eedbe31351a
Christopher Kumm <ChrisLKumm@protonmail.com>
2021-01-14 18:58:00 +0300

    Hero Animation
    - Installed gsap
    - Installed GS Dev Tools on index page
    - Hero section is now animated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,28 @@ import About from '../sections/About'
 import Contact from '../sections/Contact'
 import Footer from '../sections/Footer'
 
-const Home = () => (
-  <>
-    <SEO pathname='/'/>
-    <Hero/>
-    <Portfolio/>
-    <About/>
-    <Contact/>
-    <Footer/>
-  </>
-)
+import {gsap} from 'gsap'
+import {GSDevTools} from 'gsap/GSDevTools'
+
+gsap.registerPlugin(GSDevTools)
+
+
+const Home = () => {
+
+  // GreenSock Dev Tools
+  // https://greensock.com/docs/v2/Utilities/GSDevTools
+  // GSDevTools.create()
+
+  return (
+    <>
+      <SEO pathname='/'/>
+      <Hero/>
+      <Portfolio/>
+      <About/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
 
 export default Home
