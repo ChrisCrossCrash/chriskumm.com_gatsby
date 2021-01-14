5fca4ac8432b4f24ff02615429efb72bc79e7fcb
Christopher Kumm <ChrisLKumm@protonmail.com>
2021-01-14 22:02:30 +0300

    Hero Animation
    - Installed gsap
    - Installed GS Dev Tools on index page
    - Loading screen waits until fonts are loaded
    - Hero section is now animated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import SEO from '../components/SEO'
 import Hero from '../sections/Hero'
 import Portfolio from '../sections/Portfolio'
 import About from '../sections/About'
 import Contact from '../sections/Contact'
 import Footer from '../sections/Footer'
+import Loading from '../sections/Loading'
 
 import {gsap} from 'gsap'
 import {GSDevTools} from 'gsap/GSDevTools'
@@ -18,16 +19,31 @@ const Home = () => {
   // https://greensock.com/docs/v2/Utilities/GSDevTools
   // GSDevTools.create()
 
-  return (
-    <>
-      <SEO pathname='/'/>
-      <Hero/>
-      <Portfolio/>
-      <About/>
-      <Contact/>
-      <Footer/>
-    </>
-  )
+  const [pageLoaded, setPageLoaded] = useState(false)
+  const pageRef = useRef()
+
+  useEffect(() => {
+    document.fonts.ready.then(() => setPageLoaded(true))
+  }, [])
+
+  useEffect(() => {
+    gsap.from(pageRef.current, {opacity: 0})
+  }, [pageLoaded])
+
+  if (pageLoaded) {
+    return (
+      <div ref={pageRef}>
+        <SEO pathname='/'/>
+        <Hero/>
+        <Portfolio/>
+        <About/>
+        <Contact/>
+        <Footer/>
+      </div>
+    )
+  } else {
+    return <Loading/>
+  }
 }
 
 export default Home
---
+++ src/sections/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Loading = () => (
+  <div
+    className='animate-pulse text-2xl tracking-widest min-h-screen flex flex-col justify-center items-center'
+    style={{
+      fontFamily: 'sans-serif',
+      textTransform: 'uppercase'
+    }}
+  >
+    <h1>Loading...</h1>
+  </div>
+)
+
+export default Loading
